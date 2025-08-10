6a3c4c977c74516096203b601cd633c8c3b60aea
AxelRoman1 <asroman@espol.edu.ec>
2025-08-10 12:23:28 -0500

    test para HabitacionFamiliarTest

--- tarea_solid_uml/src/test/java/com/example/patrones/factory/HabitacionFamiliarTest.java
+++ tarea_solid_uml/src/test/java/com/example/patrones/factory/HabitacionFamiliarTest.java
@@ -12,5 +12,32 @@ public class HabitacionFamiliarTest {
         familiar.estado = EstadoHabitacion.RESERVADA;
         assertFalse(familiar.estaDisponible());
     }
+      @Test
+    void estaDisponibleRetornaTruePorDefecto() {
+        HabitacionFamiliar familiar = new HabitacionFamiliar(3, 301);
+        assertTrue(familiar.estaDisponible(), "La habitación recién creada debería estar disponible");
+    }
+
+    @Test
+    void estaDisponibleRetornaFalseSiReservada() {
+        HabitacionFamiliar familiar = new HabitacionFamiliar(3, 301);
+        familiar.estado = EstadoHabitacion.RESERVADA;
+        assertFalse(familiar.estaDisponible(), "La habitación no debería estar disponible si está reservada");
+    }
+
+    @Test
+    void mostrarDetallesNoLanzaExcepcion() {
+        HabitacionFamiliar familiar = new HabitacionFamiliar(3, 301);
+        assertDoesNotThrow(() -> familiar.mostrarDetalles(),
+            "mostrarDetalles no debería lanzar excepción");
+    }
+
+    @Test
+    void calcularPrecioLanzaUnsupportedOperation() {
+        HabitacionFamiliar familiar = new HabitacionFamiliar(3, 301);
+        assertThrows(UnsupportedOperationException.class,
+            familiar::calcularPrecio,
+            "calcularPrecio debería lanzar excepción hasta que se implemente");
+    }
 
 }
